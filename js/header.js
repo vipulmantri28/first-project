7bd9095b45c515279c54efc966efbffeb1c75a3d
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-04-17 18:48:04 +0530

    menu designed with functionaltiy

--- js/header.js
+++ js/header.js
@@ -1,7 +1,8 @@
-const menu = document.querySelector('.menu');
-function toggle(isOpen) {
-  isOpen ? menu.classList.add('menu--show') : menu.classList.remove('menu--show');
+const sliderNav = document.querySelector('.navigation__slider');
+function slide(isOpen) {
+  isOpen ? sliderNav.classList.add('isShown') : sliderNav.classList.remove('isShown');
 }
+window.addEventListener('click', slide(false))
 function dropDown(target) {
     const targetParent = target.parentElement
     if (targetParent.classList.contains('active')) {
