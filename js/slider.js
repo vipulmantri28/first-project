ad12badb286486b0f546c251a7ce177e9161e3ca
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-05-14 17:25:21 +0530

    collection slider css changes

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,14 @@
 const slider = {
     init: function() {
         const sliderWrapper = document.querySelectorAll('.collection__item');
+        let currItem;
+        sliderWrapper.forEach(item => {
+            if(item.classList.contains('current')) {
+                currItem = item;
+            }
+        })
         console.log(sliderWrapper);
+        console.log(currItem);
 
     }
 }
