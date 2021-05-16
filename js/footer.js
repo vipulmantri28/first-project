b03e55de4ea177e2e18e0b9798a08ac59a46929d
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-05-16 14:44:14 +0530

    footer functionality for mobile

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+const footerHeading = document.querySelectorAll('.footer__heading');
+
+footerHeading.forEach(heading => {
+    heading.addEventListener('click', function() {
+        heading.classList.toggle('active');
+        const list = heading.nextElementSibling;
+        if (list.style.maxHeight){
+            list.style.maxHeight = null
+        }else {
+            list.style.maxHeight = list.scrollHeight + 'px';
+        }
+    })
+})
