720257c5efeea4d27d4197bdb2544b2170f456c3
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-05-16 14:49:46 +0530

    footer js update

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,16 @@
-const footerHeading = document.querySelectorAll('.footer__heading');
+if (window.innerWidth < 1024) {
+    const footerHeading = document.querySelectorAll('.footer__heading');
+    
+    footerHeading.forEach(heading => {
+        heading.addEventListener('click', function() {
+            heading.classList.toggle('active');
+            const list = heading.nextElementSibling;
+            if (list.style.maxHeight){
+                list.style.maxHeight = null
+            }else {
+                list.style.maxHeight = list.scrollHeight + 'px';
+            }
+        })
 
-footerHeading.forEach(heading => {
-    heading.addEventListener('click', function() {
-        heading.classList.toggle('active');
-        const list = heading.nextElementSibling;
-        if (list.style.maxHeight){
-            list.style.maxHeight = null
-        }else {
-            list.style.maxHeight = list.scrollHeight + 'px';
-        }
     })
-})
+}
